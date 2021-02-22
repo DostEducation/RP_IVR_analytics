5b6be076d2f1ce569d0253cf2dcf00bc3c003e1f
satendra-sr <satendra@coloredcow.com>
2021-02-22 20:25:19 +0530

    Registration flow categories

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from api import models, db, services
 def webhook(request):
     if request.method == 'POST':
         try:
-            if (request.json['contact'] and request.json['contact']['flow_category']):
-                registration = services.RegistrationService()
-                registration.handle_registration(request.json)
+            if len(request.json['contact']) and len(request.json['contact']['flow_category']):
+                flow_category = request.json['contact']['flow_category']
+
+                # Conditions based on the flow categories
+                if flow_category == 'registration':
+                    registration = services.RegistrationService()
+                    registration.handle_registration(request.json)
+                
         except IndexError:
             print("Failed to update")
     return 'Success'
