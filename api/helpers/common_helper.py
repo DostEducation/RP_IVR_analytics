3a4c8074218abcaad160ee9e5dfd5925cad7037d
rudresh <rudresh@coloredcow.com>
2021-02-27 18:03:17 +0530

    Call logs and registration updates

--- api/helpers/common_helper.py
+++ api/helpers/common_helper.py
@@ -1,6 +1,5 @@
 def get_split_prompt_by_hyphen(data):
-    prompt = data['program_details']['categories'][0]
-    split_prompt = prompt.split('-')
+    split_prompt = data.split('-')
     return split_prompt
 
 def get_split_prompt_by_underscore(data):
@@ -12,3 +11,8 @@ def remove_last_string_separated_by(dataString, separator = '_'):
 
 def sanitize_phone_string(phoneString):
 	return phoneString.replace("tel:+", "")
+
+def fetch_by_key(key, data):
+    if key in data:
+        return data[key]
+    return None
--- api/services/call_log_service.py
+++ api/services/call_log_service.py
@@ -2,27 +2,46 @@
 from api import models, db, helpers
 
 class CallLogService:
+
     def handle_call_log(self, jsonData):
         try:
             contact = jsonData['contact']
             user_phone = contact['urn']
-            self.create_call_logs(user_phone, jsonData)
+            flow_run_uuid = self.fetch_flow_run_uuid(jsonData)
+            if flow_run_uuid:
+                self.create_call_logs(user_phone, jsonData)
+                # call_log = models.CallLog.query.get_by_flow_run_uuid(flow_run_uuid)
+                # if call_log:
+                    # check if we need this block of code
+                    # self.update_call_logs(call_log, jsonData)
+                # else:
+                    # self.create_call_logs(user_phone, jsonData)
         except IndexError:
             print("Failed to log the call details")
 
-    # Handle new user registration
+    def update_call_logs(self, call_log, jsonData):
+        try:
+            # Check if we need to update call logs
+            call_log.flow_run_uuid = self.fetch_flow_run_uuid(jsonData)
+            db.session.commit()
+        except Exception as e:
+             # Need to log this
+            return "Failed to udpate call log"
+
     def create_call_logs(self, user_phone, jsonData):
-        call_log = models.CallLog(
-            flow_run_uuid = self.fetch_flow_run_uuid(jsonData),
-            call_type =  self.fetch_call_type(jsonData),
-            scheduled_by =  self.fetch_call_scheduled_by(jsonData),
-            user_phone_number =  helpers.sanitize_phone_string(user_phone),
-            system_phone_number = helpers.sanitize_phone_string(jsonData['system_phone']),
-            # registration_id =  jsonData[''],
-            # user_id =  data[''],
-        )
-        db.session.add(call_log)
-        db.session.commit()
+        try:
+            call_log = models.CallLog(
+                flow_run_uuid = self.fetch_flow_run_uuid(jsonData),
+                call_type =  self.fetch_call_type(jsonData),
+                scheduled_by =  self.fetch_call_scheduled_by(jsonData),
+                user_phone_number =  helpers.sanitize_phone_string(user_phone),
+                system_phone_number = helpers.sanitize_phone_string(jsonData['system_phone']),
+            )
+            db.session.add(call_log)
+            db.session.commit()
+        except Exception as e:
+            # Need to log this
+            return "Failed to create call log"
 
     def fetch_flow_run_uuid(self, jsonData):
         if 'run_uuid' in jsonData:
