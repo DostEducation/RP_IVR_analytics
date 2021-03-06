1cf02d6585e8e172cb39e3cfb880e35b5f417800
satendra-sr <satendra@coloredcow.com>
2021-03-06 15:48:14 +0530

    Typo fix

--- api/helpers/common_helper.py
+++ api/helpers/common_helper.py
@@ -2,7 +2,7 @@ def remove_last_string_separated_by(dataString, separator = '_'):
     return dataString.rpartition(separator)[0]
 
 def sanitize_phone_string(phoneString):
-	""" TODO: Need to follow a common guildline throughout the project. Currently Rapid pro and IVR provides phone number in diggerent format"""
+	""" TODO: Need to follow a common guildline throughout the project. Currently Rapid pro and IVR provides phone number in different format"""
 	phoneString = phoneString.replace("+", "")
 	return phoneString.replace("tel:", "")
 
