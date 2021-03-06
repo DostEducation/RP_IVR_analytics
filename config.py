2aaf68925565e7eca7edd5a098ca823f07858346
satendra-sr <satendra@coloredcow.com>
2021-03-06 11:39:35 +0530

    Fix database socket connection issue

--- config.py
+++ config.py
@@ -16,8 +16,15 @@ POSTGRES = {
     'database': environ.get('DB_NAME'),
     'host': environ.get('DB_HOST'),
     'port': environ.get('DB_PORT'),
+    'connection_name': environ.get('CONNECTION_NAME'),
 }
-SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(database)s' % POSTGRES
+if FLASK_ENV == 'development':
+	SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(database)s' % POSTGRES
+else:
+	# For socket based connection
+	SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(password)s/%(database)s?host=/cloudsql/%(connection_name)s/'  % POSTGRES
+
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 WTF_CSRF_ENABLED = True
 SECRET_KEY = environ.get('SECRET_KEY')
