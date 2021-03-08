7a9b79eb7accac956d8d8a8b8ae6bf2bc1a95d05
satendra-sr <satendra@coloredcow.com>
2021-03-08 14:46:06 +0530

    Seperate flow for registartion

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ def webhook(request):
     if request.method == 'POST':
         try:
             jsonData = request.get_json()
-            if 'contact' in jsonData and 'run_uuid' in jsonData and 'flow_category' in jsonData:
-                flow_category = jsonData['flow_category']
+            if 'contact' in jsonData and 'run_uuid' in jsonData:
                 # Conditions based on the flow categories
-                if flow_category == 'registration':
-                    registration_service = services.RegistrationService()
-                    registration_service.handle_registration(jsonData)
+                if 'flow_category' in jsonData:
+                    handle_flow_category_data(jsonData)
                 
                 # Handle call logs
                 calllog_service = services.CallLogService()
@@ -18,9 +16,19 @@ def webhook(request):
 
                 # All the prompt responses are captured with results
                 if 'results' in jsonData:
-                    prompt_service = services.PromptService()
-                    prompt_service.handle_prompt_response(jsonData)
+                    handle_prompts(jsonData)
+            else:
+                return jsonify(message = "Contact or Flow Run UUID is missing"), 400
                 
         except IndexError:
             return jsonify(message="Invalid data"), 400
-    return 'Success'
+    return 'Currently, the system do not accept a GET request.'
+
+def handle_flow_category_data(jsonData):
+    if jsonData['flow_category'] == 'registration':
+        registration_service = services.RegistrationService()
+        registration_service.handle_registration(jsonData)
+
+def handle_prompts(jsonData):
+    prompt_service = services.PromptService()
+    prompt_service.handle_prompt_response(jsonData)
