e6944b5b717efd0ce2a40a9c3c1cf31943d8dffa
Satendra Rawat <satendra@coloredcow.com>
2021-06-21 11:55:20 +0530

    Code refactoring

--- api/helpers/prompt_helper.py
+++ api/helpers/prompt_helper.py
@@ -6,3 +6,27 @@ def split_prompt_by_hyphen(data):
 def split_prompt_by_underscore(data):
     program_sub_prompt = data.split("_")
     return program_sub_prompt
+
+
+def get_program_prompt_id(jsonData):
+    """
+    Expected program_details categories format (for ex): INTRO_1-SIGNUP_1-SELECTION_PROGRAM-OPTIN_2
+    Value returned by this function: 2
+    """
+    if "program_details" in jsonData:
+        program_categories = helpers.fetch_by_key(
+            "categories", jsonData["program_details"]
+        )
+        if len(program_categories) > 0:
+            split_prompt_by_hyphen = helpers.split_prompt_by_hyphen(
+                program_categories[0]
+            )
+            split_prompt_by_underscore = helpers.split_prompt_by_underscore(
+                split_prompt_by_hyphen[-1]
+            )
+            return (
+                split_prompt_by_underscore[1]
+                if len(split_prompt_by_underscore) > 1
+                else None
+            )
+    return None
