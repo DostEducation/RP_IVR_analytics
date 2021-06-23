4ae8de396d79236ae6afa83d81ca2af241a3035f
Satendra Rawat <satendra@coloredcow.com>
2021-06-24 00:30:46 +0530

    Fixed user program logic

--- api/models/user_program.py
+++ api/models/user_program.py
@@ -1,27 +1,33 @@
 from api.mixins import TimestampMixin
-from api import db, helpers
+from api import db, helpers, models
 from flask_sqlalchemy import BaseQuery
 
 
 class UserProgramQuery(BaseQuery):
     def upsert_user_program(self, user_id, program_id, data):
-        user_program_details = self.get_by_user_and_program_ids(user_id, program_id)
+        user_program_details = self.get_latest_active_user_program(user_id)
         if not user_program_details:
             self.create(user_id, program_id, data)
         else:
-            self.update(user_program_details, data)
+            self.update(user_program_details, program_id, data)
 
     def create(self, user_id, program_id, data):
         user_program = UserProgram(
             user_id=user_id,
             program_id=program_id,
-            status=data["status"] if "status" in data else "in-progress",
+            status=data["status"]
+            if "status" in data
+            else models.UserProgram.UserProgramStatus.IN_PROGRESS,
+            preferred_time_slot=data["preferred_time_slot"]
+            if "preferred_time_slot" in data
+            else None,
         )
         helpers.save(user_program)
 
-    def update(self, user_program_details, data):
+    def update(self, user_program_details, program_id, data):
         try:
             for key, value in data.items():
+                user_program_details.program_id = program_id
                 if key == "status":
                     user_program_details.status = value
             db.session.commit()
@@ -36,7 +42,8 @@ class UserProgramQuery(BaseQuery):
     def get_latest_active_user_program(self, user_id):
         return (
             self.filter(
-                UserProgram.user_id == user_id, UserProgram.status != "complete"
+                UserProgram.user_id == user_id,
+                UserProgram.status == models.UserProgram.UserProgramStatus.IN_PROGRESS,
             )
             .order_by(UserProgram.id.desc())
             .first()
@@ -46,6 +53,11 @@ class UserProgramQuery(BaseQuery):
 class UserProgram(TimestampMixin, db.Model):
     query_class = UserProgramQuery
     __tablename__ = "user_program"
+
+    class UserProgramStatus(object):
+        IN_PROGRESS = "in-progress"
+        COMPLETE = "complete"
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     program_id = db.Column(db.Integer, db.ForeignKey("program.id"))
