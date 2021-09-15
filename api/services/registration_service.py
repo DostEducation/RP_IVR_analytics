620132a9b63468cc3178dd74d2a95907b06b1111
satendra-sr <satendra@coloredcow.com>
2021-09-15 17:30:40 +0530

    Added check to not update the program id and status if they already exist and we got no response for user

--- api/services/registration_service.py
+++ api/services/registration_service.py
@@ -88,18 +88,21 @@ class RegistrationService(object):
         if not registration:
             return
 
-        registration.program_id = self.selected_program_id
+        if not (registration.program_id and self.has_default_program_selection):
+            registration.program_id = self.selected_program_id
 
         self.user_id = self.create_user(jsonData) if self.selected_program_id else None
-        registration.signup_date = (
-            None if self.has_default_program_selection else datetime.now()
-        )
 
-        registration.status = (
-            models.Registration.RegistrationStatus.INCOMPLETE
-            if self.has_default_program_selection
-            else models.Registration.RegistrationStatus.COMPLETE
-        )
+        if not self.has_default_program_selection:
+            registration.signup_date = datetime.now()
+
+        if (
+            not self.has_default_program_selection
+            or registration.status == models.Registration.RegistrationStatus.COMPLETE
+        ):
+            registration.status = models.Registration.RegistrationStatus.COMPLETE
+        else:
+            registration.status = models.Registration.RegistrationStatus.INCOMPLETE
 
         if self.user_id:
             registration.user_id = self.user_id
