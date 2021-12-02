e2b56fe7ead901b1f02f35fee6ee0c81a05d903f
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2021-12-02 14:31:12 +0530

    cleanups

--- main.py
+++ main.py
@@ -50,51 +50,53 @@ def retry_failed_webhook(transaction_log_service):
 
 
 def handle_payload(jsonData, is_retry_payload=False):
-    # try:
-    if "contact" in jsonData:
-        # Conditions based on the flow categories
-        if "flow_category" in jsonData and jsonData["flow_category"] != "dry_flow":
-            handle_flow_category_data(jsonData)
+    try:
+        if "contact" in jsonData:
+            # Conditions based on the flow categories
+            if "flow_category" in jsonData and jsonData["flow_category"] != "dry_flow":
+                handle_flow_category_data(jsonData)
 
-            # Handle call logs
-            calllog_service = services.CallLogService()
-            calllog_service.handle_call_log(jsonData)
+                # Handle call logs
+                calllog_service = services.CallLogService()
+                calllog_service.handle_call_log(jsonData)
 
-        # All the prompt responses are captured with results
-        if "results" in jsonData:
-            handle_prompts(jsonData)
+            # All the prompt responses are captured with results
+            if "results" in jsonData:
+                handle_prompts(jsonData)
 
-        # Handle content details
-        user_module_content_id = None
-        if "content_id" in jsonData:
-            content_service = services.ContentService()
-            user_module_content_id = content_service.add_user_module_content(jsonData)
-        if user_module_content_id:
-            calllog_service.update_user_module_content_id_in_call_log(
-                user_module_content_id
-            )
+            # Handle content details
+            user_module_content_id = None
+            if "content_id" in jsonData:
+                content_service = services.ContentService()
+                user_module_content_id = content_service.add_user_module_content(
+                    jsonData
+                )
+            if user_module_content_id:
+                calllog_service.update_user_module_content_id_in_call_log(
+                    user_module_content_id
+                )
 
-        # Handle contact groups
-        if (
-            "groups" in jsonData["contact"]
-            and jsonData["contact"]["groups"] is not None
-            and jsonData.get("flow_category", None) == "dry_flow"
-            and not is_retry_payload
-        ):
-            handle_user_group_data(jsonData)
+            # Handle contact groups
+            if (
+                "groups" in jsonData["contact"]
+                and jsonData["contact"]["groups"] is not None
+                and jsonData.get("flow_category", None) == "dry_flow"
+                and not is_retry_payload
+            ):
+                handle_user_group_data(jsonData)
 
-        # Handle custom fields
-        if (
-            "fields" in jsonData["contact"]
-            and jsonData["contact"]["fields"] is not None
-            and jsonData.get("flow_category", None) == "dry_flow"
-            and not is_retry_payload
-        ):
-            handle_user_custom_field_data(jsonData)
-    else:
-        return -1
-    # except:
-    #     return False
+            # Handle custom fields
+            if (
+                "fields" in jsonData["contact"]
+                and jsonData["contact"]["fields"] is not None
+                and jsonData.get("flow_category", None) == "dry_flow"
+                and not is_retry_payload
+            ):
+                handle_user_custom_field_data(jsonData)
+        else:
+            return -1
+    except:
+        return False
     return True
 
 
