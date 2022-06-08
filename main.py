5c820c9ebb612359d7e2b593ea6d723d3a2ccaec
Satendra Rawat <satendra@coloredcow.com>
2022-06-08 13:21:37 +0530

    Added exception to the message

--- main.py
+++ main.py
@@ -14,22 +14,25 @@ def webhook(request):
             return jsonify(message="Something went wrong!"), 400
 
         transaction_log_service = services.TransactionLogService()
+        try:
+            if jsonData and jsonData.get("type", None) != ("retry_failed_log"):
+                print("We are getting the webhook data here")
+                if "contact" in jsonData:
+                    webhook_log = transaction_log_service.create_new_webhook_log(jsonData)
+                processed = handle_payload(jsonData)
 
-        if jsonData and jsonData.get("type", None) != ("retry_failed_log"):
-            print("We are getting the webhook data here")
-            if "contact" in jsonData:
-                webhook_log = transaction_log_service.create_new_webhook_log(jsonData)
-            processed = handle_payload(jsonData)
+                if processed is False:
+                    return jsonify(message="Something went wrong!"), 400
+                elif processed == -1:
+                    return jsonify(message="Contact"), 400
 
-            if processed is False:
-                return jsonify(message="Something went wrong!"), 400
-            elif processed == -1:
-                return jsonify(message="Contact"), 400
-
-            if "contact" in jsonData:
-                transaction_log_service.mark_webhook_log_as_processed(webhook_log)
-        elif jsonData and jsonData.get("type", None) == "retry_failed_log":
-            retry_failed_webhook(transaction_log_service)
+                if "contact" in jsonData:
+                    transaction_log_service.mark_webhook_log_as_processed(webhook_log)
+            elif jsonData and jsonData.get("type", None) == "retry_failed_log":
+                retry_failed_webhook(transaction_log_service)
+        except Exception as e:
+            print(e)
+            return jsonify(message="Something went wrong!"), 400
 
         return jsonify(message="Success"), 200
     else:
