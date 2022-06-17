055f478a5e17528d2a305885ff3266f1d0b05c23
Satendra Rawat <satendra@coloredcow.com>
2022-06-17 22:20:24 +0530

    Fixed logic for -SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -22,16 +22,17 @@ POSTGRES = {
     "port": os.environ.get("DB_PORT"),
     "connection_name": os.environ.get("CONNECTION_NAME"),
 }
+
+SQLALCHEMY_DATABASE_URI = (
+    "postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(database)s" % POSTGRES
+)
+
 # For socket based connection
 if FLASK_ENV == "staging":
     SQLALCHEMY_DATABASE_URI = (
         "postgresql://%(user)s:%(password)s@/%(database)s?host=%(connection_name)s/"
         % POSTGRES
     )
-
-SQLALCHEMY_DATABASE_URI = (
-    "postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(database)s" % POSTGRES
-)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 WTF_CSRF_ENABLED = True
