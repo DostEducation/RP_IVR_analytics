9eed3f2141e8125828ec136604289f19da986de2
Gaurav Gusain <gaurav.gusain@coloredcow.in>
2022-08-17 14:00:52 +0530

    updated column name

--- api/models/registration.py
+++ api/models/registration.py
@@ -42,7 +42,7 @@ class Registration(TimestampMixin, db.Model):
     state = db.Column(db.String(100))
     education_level = db.Column(db.String(255), nullable=True)
     occupation = db.Column(db.String(255), nullable=True)
-    gender = db.Column(db.String(255), nullable=True)
+    gender_of_child = db.Column(db.String(255), nullable=True)
     area_type = db.Column(db.String(255), nullable=True)
     parent_type = db.Column(db.String(50))
     is_child_between_0_3 = db.Column(db.Boolean)
