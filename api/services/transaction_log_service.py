1a544f348e6bcd2dd29dccf4a3b8e90c8419c2ee
satendra-sr <satendra@coloredcow.com>
2022-09-17 16:27:18 +0530

    Removed update transaction logs command

--- api/services/transaction_log_service.py
+++ api/services/transaction_log_service.py
@@ -4,16 +4,12 @@ import json
 
 
 class TransactionLogService(object):
-    def create_new_webhook_log(self, jsonData):
+    def create_new_webhook_log(self, jsonData, is_processed):
         new_webhook_log = models.WebhookTransactionLog(
-            payload=json.dumps(jsonData), processed=False
+            payload=json.dumps(jsonData), processed = is_processed
         )
         helpers.save(new_webhook_log)
         return new_webhook_log
-
-    def mark_webhook_log_as_processed(self, webhook_log):
-        webhook_log.processed = True
-        helpers.save(webhook_log)
 
     def get_failed_webhook_transaction_log(self):
         failed_webhook_transaction_logs = (
