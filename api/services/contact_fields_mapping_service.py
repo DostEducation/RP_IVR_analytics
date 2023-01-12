03e74456140dd8902f41cb7089442c4c132aeed8
Satendra Rawat <satendra@coloredcow.com>
2023-01-12 14:04:39 +0530

    Added functionality to mark user program unsub or complete with custom field mapping

--- api/services/contact_fields_mapping_service.py
+++ api/services/contact_fields_mapping_service.py
@@ -101,7 +101,8 @@ class ContactFieldsMappingService(object):
         try:
             table_data = table_object.get_by_user_id(user_details.id)
             if table_data:
-                setattr(table_data, column_name, mapped_table_column_value)
-                db.session.commit()
+                if table_data.status != mapped_table_column_value:
+                    setattr(table_data, column_name, mapped_table_column_value)
+                    db.session.commit()
         except Exception as e:
             print(f"Exception occurred: {e}")
