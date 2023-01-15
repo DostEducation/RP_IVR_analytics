f41cd9d34581963b85bd7d9a8f6743bf69883567
Satendra Rawat <satendra@coloredcow.com>
2023-01-15 20:43:32 +0530

    Changes in logic to update user data from custom fields and groups

--- api/models/contact_fields_mapping.py
+++ api/models/contact_fields_mapping.py
@@ -18,7 +18,7 @@ class ContactFieldsMappingQuery(BaseQuery):
         except Exception as e:
             print(f"Exception occurred: {e}")
 
-    def get_all_campaign_mapping(self):
+    def get_all_contact_fields_mapping(self):
         return db.session.query(ContactFieldsMapping).all()
 
 
