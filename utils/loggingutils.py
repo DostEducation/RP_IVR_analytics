42100cd7968bdb4b8d43d9882425aa3e6f124281
Satendra Rawat <satendra@coloredcow.com>
2023-02-08 15:25:57 +0530

    Generating different level of logs in the cloud function

--- utils/loggingutils.py
+++ utils/loggingutils.py
@@ -5,11 +5,11 @@ from google.cloud import logging as gcloud_logging
 logger = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG)
 
-if os.getenv("FLASK_ENV") == "production":
+if os.getenv("FLASK_ENV") == "development":
+    log_handler = logging.getLogger().handlers[0]
+    app.logger.addHandler(log_handler)
+else:
     log_client = gcloud_logging.Client()
     log_client.setup_logging()
     log_handler = log_client.get_default_handler()
-    app.logger.addHandler(log_handler)
-else:
-    log_handler = logging.getLogger().handlers[0]
     app.logger.addHandler(log_handler)
