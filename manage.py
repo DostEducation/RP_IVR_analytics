a1cdbab0cfb277393ef8ae9470fd6b95b0eec293
Sachin Bisht <sachinbisht27@gmail.com>
2023-09-27 16:48:06 +0530

    added flask app name in the migration flow

--- manage.py
+++ manage.py
@@ -3,11 +3,13 @@ from api import db, app
 from api.models import *
 
 
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
 from flask.cli import FlaskGroup
 
 migrate = Migrate(app, db)
 cli = FlaskGroup(app)
+
+cli.add_command("db", MigrateCommand)
 
 if __name__ == "__main__":
     cli()
