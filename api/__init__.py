3b550e4a4676ef2628b6ec212de1849ace051c00
Sachin Bisht <sachinbisht27@gmail.com>
2023-10-02 18:42:35 +0530

    Printing the databse url

--- api/__init__.py
+++ api/__init__.py
@@ -4,5 +4,3 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object("config")
 db = SQLAlchemy(app)
-
-print("DATABASE_URL:", app.config["SQLALCHEMY_DATABASE_URI"])
