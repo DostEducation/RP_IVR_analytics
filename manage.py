ed68ad9ea6d10f71136c6ae20a666c0c57899f78
Sachin Bisht <sachinbisht27@gmail.com>
2023-10-05 16:37:19 +0530

    Implemented the flask cli method to manage migrations

--- manage.py
+++ manage.py
@@ -3,13 +3,11 @@ from api import app, db
 from api.models import *
 
 
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask_migrate import Migrate
+from flask.cli import FlaskGroup
 
 migrate = Migrate(app, db)
-
-manager = Manager(app)
-manager.add_command("db", MigrateCommand)
+cli = FlaskGroup(app)
 
 if __name__ == "__main__":
-    manager.run()
+    cli()
