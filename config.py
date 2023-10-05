78f5a06e5e35c540121c43a14f8a3b9bbab533d7
Sachin Bisht <sachinbisht27@gmail.com>
2023-10-05 20:01:26 +0530

    Checking files in the current directory

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ if FLASK_ENV == "staging":
         "postgresql://%(user)s:%(password)s@/%(database)s?host=%(connection_name)s/"
         % POSTGRES
     )
-print(SQLALCHEMY_DATABASE_URI)
+print("POSTGRES", POSTGRES)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 WTF_CSRF_ENABLED = True
