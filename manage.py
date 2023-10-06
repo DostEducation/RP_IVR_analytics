bf325928ca8a2178ce41e8c60b955678333e5e69
Sachin Bisht <sachinbisht27@gmail.com>
2023-10-06 20:04:21 +0530

    Changed the implementation for the database migration

--- manage.py
+++ manage.py
@@ -5,6 +5,8 @@ from api.models import *
 
 from flask_migrate import Migrate
 from flask_script import Manager
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 
 migrate = Migrate(app, db)
 
@@ -12,3 +14,22 @@ manager = Manager(app)
 
 if __name__ == "__main__":
     manager.run()
+
+SQLALCHEMY_DATABASE_URI = app.config["SQLALCHEMY_DATABASE_URI"]
+
+try:
+    # Create a SQLAlchemy engine using the database URI
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
+    # Attempt to establish a connection to the database
+    connection = engine.connect()
+
+    # If the connection was successful, print a success message
+    print("Database connection successful!")
+
+    # Close the connection
+    connection.close()
+
+except OperationalError as e:
+    # Handle any exceptions that may occur during the connection attempt
+    print(f"Error connecting to the database: {str(e)}")
