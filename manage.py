82a07829ab3379ce57573de120e2ecadb75fbf1c
Sachin Bisht <sachinbisht27@gmail.com>
2023-10-09 19:06:42 +0530

    testing connection to the database

--- manage.py
+++ manage.py
@@ -3,13 +3,10 @@ from api import db, app
 from api.models import *
 from utils.loggingutils import logger
 
-
 from flask_migrate import Migrate
 from flask_script import Manager
-from sqlalchemy import create_engine
 
 migrate = Migrate(app, db)
-
 manager = Manager(app)
 
 if __name__ == "__main__":
@@ -17,17 +14,7 @@ if __name__ == "__main__":
 
 SQLALCHEMY_DATABASE_URI = app.config["SQLALCHEMY_DATABASE_URI"]
 
-
-def select_user_phone_from_db(connection):
-    try:
-        result = connection.execute("SELECT phone FROM users")
-        # Fetch all the rows from the result set
-        user_data = result.fetchall()
-        return user_data
-    except Exception as e:
-        logger.error(f"Error occurred while fetching data. Error Message: {e}")
-        return None
-
+from sqlalchemy import create_engine, text
 
 try:
     # Create a SQLAlchemy engine using the database URI
@@ -35,11 +22,18 @@ try:
 
     # Attempt to establish a connection to the database
     connection = engine.connect()
-    cursor = connection.cursor()
 
     # If the connection was successful, print a success message
     logger.info(f"Database connection successful on URI {SQLALCHEMY_DATABASE_URI}")
-    user_data = select_user_phone_from_db(connection)
+
+    # Use SQLAlchemy's text function to create an SQL statement
+    sql_statement = text("SELECT phone FROM users")
+
+    # Execute the SQL statement
+    result = connection.execute(sql_statement)
+
+    # Fetch all the rows from the result set
+    user_data = result.fetchall()
     logger.info(f"user data: {user_data}")
 
     # Close the connection
