e05050ae209c2f45719a6d8d3238152fd2461acf
gourabdatabridge <gourab@dosteducation.com>
2024-01-12 12:50:25 +0530

    Minor debugging

--- api/models/registration.py
+++ api/models/registration.py
@@ -1,8 +1,8 @@
 from api.mixins import TimestampMixin
 from api import db
 
-# from flask_sqlalchemy import BaseQuery
-from flask_sqlalchemy import BaseQuery
+# from flask_sqlalchemy.query import Query as BaseQuery
+from flask_sqlalchemy.query import Query as BaseQuery
 
 
 class RegistrationQuery(BaseQuery):
